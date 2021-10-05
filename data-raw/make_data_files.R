5a315dbfbe442e59bd71b0904b02eeb90bcd8b9c
Andrew Lamb <andrewelamb@gmail.com>
2021-10-05 06:03:05 -0700

    export cohort rda objects

---
+++ data-raw/make_data_files.R
@@ -0,0 +1,95 @@
+cnf1 <- CohortNumericFilter$new(
+  "name" = "B_cells_Aggregate2",
+  "min" = 0,
+  "max" = .01
+)
+
+cnf2 <- CohortNumericFilter$new(
+  "name" = "Neutrophils_Aggregate2",
+  "min" = 0,
+  "max" = .1
+)
+
+cnfs1 <- CohortFilterList$new(list(), type = "numeric")
+cnfs2 <- CohortFilterList$new(list(cnf1), type = "numeric")
+
+cgf1 <- CohortGroupFilter$new(
+  "name" = "PCAWG_Study",
+  "values" = c("BLCA-US", "BRCA-US", "CLLE-ES")
+)
+
+cgf2 <- CohortGroupFilter$new(
+  "name" = "Immune_Subtype",
+  "values" = c("C4", "C5", "C6")
+)
+
+cgfs1 <- CohortFilterList$new(list(), type = "group")
+cgfs2 <- CohortFilterList$new(list(cgf2), type = "group")
+
+
+cf1 <- CohortFilters$new(
+  numeric_filters = cnfs1,
+  group_filters = cgfs1
+)
+
+cf2 <- CohortFilters$new(
+  numeric_filters = cnfs2,
+  group_filters = cgfs2
+)
+
+tag_group1 <- TagGroup$new(
+  dataset_names = "PCAWG", group_name = "Immune_Subtype"
+)
+
+tag_group2 <- TagGroup$new(
+  dataset_names = "TCGA", group_name = "Immune_Subtype"
+)
+
+tag_group3 <- TagGroup$new(
+  dataset_names = c("Gide_Cell_2019", "HugoLo_IPRES_2016"), group_name = "Responder"
+)
+
+mutation_group <- MutationStatusGroup$new(
+  dataset_names = "TCGA", mutation_name = "ABL1:(NS)"
+)
+
+bin_group <- FeatureBinGroup$new(
+  dataset_names = "TCGA", feature_name = "B_cells_Aggregate2", feature_bins = 2
+)
+
+
+pcawg_immune_subtype_cohort_obj <- Cohort$new(
+  "filter_object" = cf1,
+  "group_object" = tag_group1
+)
+
+usethis::use_data(pcawg_immune_subtype_cohort_obj, overwrite = TRUE)
+
+tcga_immune_subtype_cohort_obj_small <- Cohort$new(
+  "filter_object" = cf2,
+  "group_object" = tag_group2
+)
+
+usethis::use_data(tcga_immune_subtype_cohort_obj_small, overwrite = TRUE)
+
+ici_responder_cohort_obj <- Cohort$new(
+  "filter_object" = cf1,
+  "group_object" = tag_group3
+)
+
+usethis::use_data(ici_responder_cohort_obj, overwrite = TRUE)
+
+tcga_mutation_cohort_obj_small <- Cohort$new(
+  "filter_object" = cf2,
+  "group_object" = mutation_group
+)
+
+usethis::use_data(tcga_mutation_cohort_obj_small, overwrite = TRUE)
+
+tcga_bin_cohort_obj_small <- Cohort$new(
+  "filter_object" = cf2,
+  "group_object" = bin_group
+)
+
+usethis::use_data(tcga_bin_cohort_obj_small, overwrite = TRUE)
+
